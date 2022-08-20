39f47613c14cf6fa75e4507b1466f6288c4abf91
Richard Prandt <rprandt.contato@gmail.com>
2022-08-20 11:36:35 -0300

    fisrt commit

---
+++ src/main/java/com/rprandt/store/domain/Product.java
@@ -0,0 +1,12 @@
+package com.rprandt.store.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Product {
+    private String name;
+    private Long value;
+    private Long amout;
+}
