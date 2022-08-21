d29a658ba1e3ca261bd84d1b4c606d4b841dc6ce
Richard Prandt <rprandt.contato@gmail.com>
2022-08-21 13:24:28 -0300

    Some changes

--- src/main/java/com/rprandt/store/config/ResourceServerConfig.java
+++ src/main/java/com/rprandt/store/config/ResourceServerConfig.java
@@ -32,7 +32,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Autowired
 	private JwtTokenStore tokenStore;
 	
-	private static final String[] PUBLIC = { "/oauth/token", "/users/create" };
+	private static final String[] PUBLIC = { "/oauth/token", "/oauth/users/create" };
 
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
--- src/main/java/com/rprandt/store/resource/OAuthResource.java
+++ src/main/java/com/rprandt/store/resource/OAuthResource.java
@@ -1,23 +1,22 @@
 package com.rprandt.store.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rprandt.store.domain.User;
 import com.rprandt.store.service.UserService;
 
-
 @RestController
-@RequestMapping(value="/users")
-public class UserResource {
+@RequestMapping("/oauth")
+public class OAuthResource {
     @Autowired
-    private UserService service;
+    private UserService userService;
 
-    @GetMapping("/{id}")
-    public User find(@PathVariable String id) {
-       return service.find(id);
+    @PostMapping("/user/create")
+    public void save(@RequestBody User obj){
+        userService.save(obj);
     }
 }
